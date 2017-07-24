4f67c9f285c8199dfb95c49033c49086560eb12f
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-24 15:21:52 -0400

    Add JQ slide toggle to sections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-jQuery(document).ready(function(){
+//JQ Task #1, to toggle up the sections to collapse upon a click
 
+$( document ).ready(function() {
+    $( '#testimonials' ).click(function() {
+        $( '.testToggle' ).slideToggle();
+     });
+     $( '#skills-Toggle' ).click(function() {
+         $( '.testToggle2' ).slideToggle();
+      });
+      $( '#education-Toggle' ).click(function() {
+          $( '.testToggle3' ).slideToggle();
+       });
+       $( '#work-Toggle' ).click(function() {
+           $( '.testToggle4' ).slideToggle();
+        });
 });
