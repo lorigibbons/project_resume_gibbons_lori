7b96c6d0a0102a32de0c766db9a0a1e9f281fde7
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-07-24 19:16:39 -0400

    Correct linter errors in js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 //JQ Task #1, to toggle up the sections to collapse upon a click
 
-$( document ).ready(function() {
-    $( '#testimonials' ).click(function() {
-        $( '.testToggle' ).slideToggle();
-     });
-     $( '#skills-Toggle' ).click(function() {
-         $( '.testToggle2' ).slideToggle();
-      });
-      $( '#education-Toggle' ).click(function() {
-          $( '.testToggle3' ).slideToggle();
-       });
-       $( '#work-Toggle' ).click(function() {
-           $( '.testToggle4' ).slideToggle();
-        });
+$(document).ready(function () {
+    $('#testimonials').click(function () {
+        $('.testToggle').slideToggle();
+    });
+    $('#skills-Toggle').click(function () {
+        $('.testToggle2').slideToggle();
+    });
+    $('#education-Toggle').click(function () {
+        $('.testToggle3').slideToggle();
+    $('#work-Toggle').click(function () {
+        $('.testToggle4').slideToggle();
+    });
 });
