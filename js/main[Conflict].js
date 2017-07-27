a2fcbbbd43a2930ec902b10013356fa0d9fe58b3
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-27 13:16:51 -0400

    Fix linter errors and spacings

---
+++ js/main[Conflict].js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    //^^^this means js will wait for html to load then run
+
+    //JQ Task #1, to toggle up the sections to collapse upon a click
+    $('#testimonials').click(function () {
+        $('.testToggle').slideToggle();
+    });
+    $('#skills-Toggle').click(function () {
+        $('.testToggle2').slideToggle();
+    });
+    $('#education-Toggle').click(function () {
+        $('.testToggle3').slideToggle();
+    });
+    $('#work-Toggle').click(function () {
+        $('.testToggle4').slideToggle();
+    });
+
+//https://code.google.com/archive/p/jquery-tubular/
+
+//Point Point Plugin
+    $('#pushButton').pointPoint();
+
+/*
+    // You can also pass arguments:
+    $('#pushButton').pointPoint({
+        "class":"myNewPointer",
+        "distance":100
+    });
+*/
+
+//CIRCLE TEXT test  http://www.jqueryscript.net/text/jQuery-Plugin-For-Flexible-Responsive-Curving-Text-CircleType-js.html
+//http://circletype.labwire.ca/
+
+    $('#demo').circleType({radius: 384});
+
+//LETTERING test  https://github.com/davatron5000/Lettering.js
+    $('.fancy_title').lettering();
+
+});
